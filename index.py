c15c94852ee88c2cac7fac267c89a072627d24f9
Casey Link <unnamedrambler@gmail.com>
2011-03-22 23:24:59 -0400

    add barebones hello world rest server

---
+++ index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import web
+
+urls = ( '/', 'index' )
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run()
