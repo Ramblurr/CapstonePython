496a1a06951208906b149d7878aeeb845aab4d0c
Casey Link <unnamedrambler@gmail.com>
2011-03-23 02:35:10 -0400

    Add basic HTML form with some nics css

--- index.py
+++ index.py
@@ -2,14 +2,62 @@
 # -*- coding: utf-8 -*-
 
 import web
+import os, glob
 
-urls = ( '/', 'index' )
-
+urls = ( '/', 'index',
+         '/res/(.*)', 'static')
+render = web.template.render('resources/')
 app = web.application(urls, globals())
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        return render.index("hi")
+
+class static:
+    # static whitelist
+    def __init__(self):
+        self.whitelist = [ 'resources/main.css', 'resources/images/*' ]
+
+    def in_whitelist(self, path):
+        print "'%s'" % (path)
+        for items in self.whitelist:
+            print "'%s'" % (items)
+            for item in glob.glob(items):
+                print item,path
+                if item == path:
+                    return True
+        return False
+
+    def GET(self, path):
+
+        if not path: # index
+            path = "index.html"
+        base = "resources"
+        full_path = os.path.join( base, path )
+        print "path " + full_path
+        if not self.in_whitelist(full_path):
+            return web.notfound("Sorry, the page you were looking for was not found.")
+        # - static handler
+        #Copyright (c) 2006-2009, Wade Alcorn
+        #All Rights Reserved
+        #wade@bindshell.net - http://www.bindshell.net
+        #
+        #   Path traversal protection
+        #
+        full_path = os.path.join(os.getcwd(), full_path)
+        if not full_path.startswith( os.getcwd() ):
+            return web.notfound("Sorry, the page you were looking for was not found.")
+        if os.path.exists( full_path ) and os.path.isfile( full_path ):
+            if os.access( full_path, os.R_OK ):
+                fh = file( full_path, 'r')
+                return fh.read()
+            else:
+                #
+                #   The user has no privileges to read this file
+                #
+                return web.notfound("Sorry, the page you were looking for was not found.")
+
+        return web.notfound("Sorry, the page you were looking for was not found.")
 
 
 if __name__ == "__main__":
