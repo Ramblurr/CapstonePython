35e9e1c9079e890f124bc56b7e7ba34a6cdd9444
Casey Link <unnamedrambler@gmail.com>
2011-03-22 23:10:59 -0400

    make the nasdaq parser return a dictionary for easier use.
    also add documentation to the module.

--- nasdaq/parser.py
+++ nasdaq/parser.py
@@ -1,3 +1,28 @@
+"""
+Parser for the NASDAQ dataset.
+
+This class acts like a list iterator for the NASDAQ dataset. It transparently 
+wraps all the different .csv files in the dataset.
+
+Example usage:
+from nasdaq import parser
+
+p = parser.Parser("data/NASDAQ")
+    for record in p:
+        print record
+
+Example output:
+{'price_high': '2.77', 'exchange': 'NASDAQ', 'symbol': 'ABXA', 'price_close': '2.67', 'volum
+e': '158500', 'price_open': '2.55', 'date': '2009-12-09', 'price_adj_close': '2.67', 'price_
+low': '2.50'}
+{'price_high': '2.74', 'exchange': 'NASDAQ', 'symbol': 'ABXA', 'price_close': '2.55', 'volum
+e': '131700', 'price_open': '2.71', 'date': '2009-12-08', 'price_adj_close': '2.55', 'price_
+low': '2.52'}
+...
+
+For performance reasons the len() function is not implemented.
+
+"""
 import csv
 import collections
 from string import ascii_uppercase
@@ -17,11 +42,22 @@ class Parser(collections.Iterator):
     def __iter__(self):
         return self
 
+    def _wrap_record(self,  record):
+        """
+        Turns the record into a dictionary
+        """
+        keys = ['exchange', 'symbol', 'date', 'price_open', 'price_high', 'price_low', 'price_close', 'volume', 'price_adj_close']
+        return dict(zip(keys,  record))
+
     def next(self):
         try:
-            return self.csv_iter.next()
+            rec = self.csv_iter.next()
+            if rec[0] == "exchange": # this skips the header at the beginning of every csv
+                rec = self.csv_iter.next()
+            return self._wrap_record(rec)
         except StopIteration:
             self._open_next()
-            return self.csv_iter.next()
-            
-        
+            rec = self.csv_iter.next()
+            if rec[0] == "exchange":
+                rec = self.csv_iter.next()
+            return self._wrap_record(rec)
