0a911ccfa0e52065b4c36ceafbadcd6ace70dd30
Casey Link <unnamedrambler@gmail.com>
2011-03-24 19:38:51 -0400

    add the edit ip script. don't want to have to rewrite it again

---
+++ edit_ip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import socket
+import yaml
+import os
+
+ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0]
+
+config_file = "/etc/cassandra/cassandra.yaml"
+stream = file(config_file, 'r')
+config = yaml.load(stream)
+stream.close()
+
+config['listen_address'] = ip
+stream = file(config_file, 'w')
+yaml.dump(config, stream, default_flow_style=False)
+
+os.system("curl -d \"ip=%s\" 10.242.223.237:8080/seed" % (ip))
