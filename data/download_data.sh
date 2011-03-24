a4e988d2532c63a47716e37d8519b256824e71df
David Mazary <dmaz@vt.edu>
2011-03-24 13:33:33 -0400

    Updated nasdaq data URL Signed-off-by: David Mazary <dmaz@vt.edu>

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file="https://s3.amazonaws.com/infochimps/data/4777/16185/20100217033442/infochimps_dataset_4777_download_16185-csv.tar.bz2?AWSAccessKeyId=02S6Y1EFA7ZZ7KCZH3G2&Expires=1300847693&Signature=V7z6nfwkMsjmHYNiGmGoP014kcE%3D"
+file="https://s3.amazonaws.com/infochimps/data/4777/16185/20100217033442/infochimps_dataset_4777_download_16185-csv.tar.bz2?AWSAccessKeyId=02S6Y1EFA7ZZ7KCZH3G2&Expires=1300987450&Signature=ZjrTQDwC2xss3YlogRbpfol7Reo%3D"
 
 echo "Downloading dataset"
 wget $file -O nasdaq-data.tar.bz2
