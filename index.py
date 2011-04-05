189f9dec8c5b7a89d79a7e12051a2d011deac111
Jonathan Berkhahn <jberkhah@n21.(none)>
2011-04-05 18:43:10 -0400

    derp 2

--- index.py
+++ index.py
@@ -47,18 +47,18 @@ class index:
 
     def getMonth(self, x):
         return {
-            '01': "Jan",        
-	    '02': "Feb",
-            '03': "Mar",
-            '04': "Apr",
-            '05': "May",
-            '06': "Jun",
-            '07': "Jul",
-            '08': "Aug",
-            '09': "Sep",
-            '10': "Oct",
-            '11': "Nov",
-            '12': "Dec",
+            1: "Jan",        
+	    2: "Feb",
+            2: "Mar",
+            3: "Apr",
+            4: "May",
+            5: "Jun",
+            7: "Jul",
+            8: "Aug",
+            9: "Sep",
+            10: "Oct",
+            11: "Nov",
+            12: "Dec",
         }.get(x, "ERROR")
 
 
