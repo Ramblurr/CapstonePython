aa3d9e76ec8b2d9efe417dbc0db6238888ba8b56
Casey Link <unnamedrambler@gmail.com>
2011-04-07 18:15:37 -0400

    hook up the autocomplete to make it work

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import web
-import os, glob, time
+import os, glob, time, json, urlparse
 from datetime import datetime
 from web import form
 import cassandrabase
@@ -45,9 +45,16 @@ class seed:
 class symbol:
     GET = web.autodelegate('GET_')
     def GET_search(self):
-        print "Symbols"
+        return "Symbols"
     def GET_search(self, args):
-        print args
+        qs = urlparse.parse_qs(web.ctx.query[1:])
+        if 'term' not in qs:
+            return "Error"
+        term = qs['term'][0]
+        cass = cassandrabase.CassandraBase()
+        cass.connect(get_seed())
+        results = cass.get_symbols_by_partial(term)
+        return json.dumps(results)
 class index:
     def GET(self):
         return render.index("hi")
