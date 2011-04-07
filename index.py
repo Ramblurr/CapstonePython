203b5b77f0d0f761968a88534379b86eed681342
Jonathan Berkhahn <jberkhah@n21.(none)>
2011-04-07 15:29:43 -0400

    changed line segment calculation

--- index.py
+++ index.py
@@ -139,7 +139,7 @@ class index:
 
 	left_axis.append(y_min_foo)
 	lines = len(left_axis)-1
-	chart.set_grid(0, lines-3, 1, 1)
+	chart.set_grid(0, delta_y, 1, 1)
 	left_axis[0] = len(left_axis)	
 
 	x_labels = []
