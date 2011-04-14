135f178254cabe908cb4627e94a9f71a3a2239a2
Casey Link <unnamedrambler@gmail.com>
2011-04-14 16:11:54 -0400

    fix indentation errors

--- settings.py
+++ settings.py
@@ -6,19 +6,19 @@ class CassandraSettings(object):
         self.keyspace = keyspace
 
 class HbaseSettings(object):
-    def__init__(self, host):
+    def __init__(self, host):
         self.host = host
 
 class Settings(object):
     def __init__(self):
         self.config = ConfigParser.ConfigParser()
         self.config.read("settings.cfg")
-        
+
         if self.config.has_section("Cassandra"):
             self.cassandra = CassandraSettings(self.config.get("Cassandra", "host"), self.config.get("Cassandra", "keyspace") )
 
-	if self.config.has_section("Hbase"):
-	    self.hbase = HbaseSettings(self.config.get("Hbase", "host"))
-            
+        if self.config.has_section("Hbase"):
+            self.hbase = HbaseSettings(self.config.get("Hbase", "host"))
+
         if self.config.has_section("data"):
             self.data_path = self.config.get("data", "path")
